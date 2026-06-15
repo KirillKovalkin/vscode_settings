485b6b7b6576588d6ccd1c5fb73810896a295154
Kirill Kovalkin <108697657+KirillKovalkin@users.noreply.github.com>
2026-06-15 08:47:59 +0300

    Update install_extensions.sh

--- install_extensions.sh
+++ install_extensions.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-code --install-extension Continue.continue
-code --install-extension formulahendry.code-runner
-code --install-extension dracula-theme.theme-dracula
-code --install-extension usernamehw.errorlens
-code --install-extension PKief.material-icon-theme
-code --install-extension ms-python.vscode-pylance
-code --install-extension ms-python.python
-code --install-extension ms-python.debugpy
-code --install-extension ms-python.vscode-python-envs
 code --install-extension charliermarsh.ruff
+code --install-extension Continue.continue
+code --install-extension dracula-theme.theme-dracula
+code --install-extension formulahendry.code-runner
+code --install-extension ms-python.debugpy
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-python.vscode-python-envs
+code --install-extension PKief.material-icon-theme
+code --install-extension usernamehw.errorlens
 
 rm ~/.config/Code/User/settings.json
 cp settings.json ~/.config/Code/User/
